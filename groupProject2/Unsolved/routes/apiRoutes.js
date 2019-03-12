3ad99a24b6ac744e55bb38233947b347d8555565
unknown <amandastantonkc@gmail.com>
2019-03-11 21:13:54 -0500

    api-routes

--- groupProject2/Unsolved/routes/apiRoutes.js
+++ groupProject2/Unsolved/routes/apiRoutes.js
@@ -1,24 +1,28 @@
+// Grabbing our models
+
 var db = require("../models");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+
+  // GET route for getting all of the todos
+  app.get("/api/todos", function(req, res) {
+
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // POST route for saving a new todo. You can create a todo using the data on req.body
+  app.post("/api/todos", function(req, res) {
+
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // DELETE route for deleting todos. You can access the todo's id in req.params.id
+  app.delete("/api/todos/:id", function(req, res) {
+
+  });
+
+  // PUT route for updating todos. The updated todo will be available in req.body
+  app.put("/api/todos", function(req, res) {
+
   });
 };
